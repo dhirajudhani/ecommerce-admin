c3833f8e29145deabb68dc8724b52126ff33cea7
dhirajudhani <dhirajudhani870@gmail.com>
2024-10-20 10:59:02 +0530

    Environmet setup Admin

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-5">
+        <Button size="default" variant="default">Clck me</Button> 
+    </div>
+  );
+}
