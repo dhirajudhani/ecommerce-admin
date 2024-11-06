3e4b2d6790ae7a5432384f293d4ffdfe9c1015f8
dhirajudhani <dhirajudhani870@gmail.com>
2024-11-06 23:22:44 +0530

    created popover trigger

--- components/StoreSwitcher.tsx
+++ components/StoreSwitcher.tsx
@@ -1,8 +1,55 @@
+"use client"
 
-export default function StoreSwitcher() {
+import type { Store } from "@prisma/client"
+import { Popover, PopoverTrigger } from "./ui/popover"
+import { useStoreModal } from "@/hooks/use-store-modal"
+import { useParams, useRouter } from "next/navigation"
+import { useState } from "react"
+import { Button } from "./ui/button"
+import { ChevronsUpDown, Store as StoreIcon } from "lucide-react"
+import { cn } from "@/lib/utils"
+
+type PopoverTriggerProps = React.ComponentPropsWithRef<typeof PopoverTrigger>
+
+interface StoreSwitcherProps extends PopoverTriggerProps {
+    items : Store[],
+
+}
+
+
+export default function StoreSwitcher({
+    className,
+    items = []
+} : StoreSwitcherProps) {
+
+    const storeModal = useStoreModal();
+    const params = useParams();
+    const router = useRouter();
+
+    const formattedItems = items.map((item) => ({
+        label : item.name,
+        value : item.id
+    }))
+
+    const currentStore = formattedItems.find((item) => item.value === params.storeId)
+
+    const [open, setOpen] = useState(false);
+
+    const onStoreSelect = (store : {value: string, label : string}) => {
+        setOpen(true);
+        router.push(`/${store.value}`);
+    }
+
     return (
-        <div>
-            Store Switcher
-        </div>
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button variant="outline" size="sm" role="combobox" aria-expanded={open} aria-label="Select a store"
+                className={cn("w-[200px] justify-between", className)}>
+                    <StoreIcon className="mr-2 h-4 w-4"/>
+                    Current Store
+                    <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50"/>
+                </Button>
+            </PopoverTrigger>
+        </Popover>
     )
 }
