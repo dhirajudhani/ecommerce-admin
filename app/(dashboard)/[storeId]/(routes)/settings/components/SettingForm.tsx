5781617b0ba3f666afb318c4def0d226c7fdb0e6
dhirajudhani <dhirajudhani870@gmail.com>
2024-11-07 23:14:59 +0530

    update setting forms

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingForm.tsx
@@ -1,24 +1,78 @@
-"use client"
-import { Button } from "@/components/ui/button"
-import { Heading } from "@/components/ui/heading"
-import { Store } from "@prisma/client"
-import { Trash } from "lucide-react"
-import React from "react"
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Heading } from "@/components/ui/heading";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 
 interface SettingFormProps {
-    initialData : Store
+  initialData: Store;
 }
-export  const SettingForm : React.FC<SettingFormProps> = ({
-    initialData
-}) => {
-    return (
-        <>
-        <div className="flex items-center justify-between">
-            <Heading title="Setting" description="Manage store preference"/>
-            <Button variant="destructive" size="sm" onClick={() => {}}>
-                <Trash className="h-4 w-4"/>
-            </Button>
-        </div>
-        </>
-    )
-}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+type SettingFormValues = z.infer<typeof formSchema>;
+
+export const SettingForm: React.FC<SettingFormProps> = ({ initialData }) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<SettingFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (data: SettingFormValues) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Setting" description="Manage store preference" />
+        <Button disabled={loading} variant="destructive" size="sm" onClick={() => setOpen(true)}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input disabled={loading} placeholder="Store Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto"  type="submit">Save changes</Button>
+        </form>
+      </Form>
+    </>
+  );
+};
