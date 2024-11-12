d3d719d6ec971cf08b7b139e1aab88028ffb99da
dhirajudhani <dhirajudhani870@gmail.com>
2024-11-12 08:25:56 +0530

    feature: billboard creation

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -0,0 +1,12 @@
+const BillboardPage = async ({
+    params
+}: {
+    params : {billboardId : string}
+}) => {
+    return ( 
+      <div>
+        This is form for Billboard
+      </div>
+    )
+}
+export default BillboardPage;
